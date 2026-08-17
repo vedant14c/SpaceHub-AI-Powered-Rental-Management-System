ee9778745a4bdd63210ea91ca62cd27e85ecb734
Vedant <2k18vedant@gmail.com>
2026-08-17 17:27:26 +0530

    feat: configure api network client and add gitignore patterns

--- OFFICE_SPACES/app/src/main/java/com/myapplication/office_spaces/network/ApiClient.java
+++ OFFICE_SPACES/app/src/main/java/com/myapplication/office_spaces/network/ApiClient.java
@@ -15,7 +15,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public  static final String BASE_URL = "http://192.168.0.111:8080/";
+    public  static final String BASE_URL = "http://192.168.0.110:8080/";
 
     private static Retrofit retrofit;
 
